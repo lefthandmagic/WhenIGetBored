4fdee9ccd217cd6bfe798660b1442dfb01582a25
praveen <praveen@praveen-laptop>
2012-02-25 18:31:43 -0800

    First Hello World on Eclipse

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * First eclipse test project
+ * 
+ * @author praveen murugesan
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
+
+}
