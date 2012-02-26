4844aa070ddc7af112501fc96e79ae92b153913c
praveen <praveen@praveen-laptop>
2012-02-25 19:43:49 -0800

    Text Analyzer which solves the following problem.
    The application must take as input a text file, analyze the text and
    output a report.  The report should contain a count of how many times
    each word occurs in the file.  The output should be sorted, with a
    primary sort of word length, and a secondary ASCII sort.
    
    This solution uses the TreeMap java collection, 
    Solution without using java collections coming soon.

---
+++ TextAnalyzer/src/TextAnalyzer.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+/**
+ * This class defines a text analyzer which
+ * takes an input file, analyzes it and provides
+ * a report on the contents of the file.
+ * 
+ * @author praveen
+ *
+ */
+public class TextAnalyzer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = null;
+		Map<String, Integer> tokenToCountMap = new TreeMap<String, Integer>(new TextAnalyzerComparator());
+		 try {
+			scanner = new Scanner(new BufferedReader(new FileReader("testfiles/test1")));
+			
+			while(scanner.hasNext()) {
+				String token = scanner.next();
+				
+				// if the token previously exists, update the count and re-insert
+				if(tokenToCountMap.containsKey(token)) {
+					Integer currentCount = tokenToCountMap.get(token);
+					tokenToCountMap.put(token, ++currentCount);
+				} else {
+					// if the token isn't present, add it with a count 1
+					tokenToCountMap.put(token, 1);
+				}
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("Oops, please provide the correct filename");
+		} finally {
+			if(scanner != null) {
+				scanner.close();
+			}
+		}
+		 
+		printTextAnalyzerReport(tokenToCountMap);
+
+	}
+
+	/**
+	 * This helper method is used to print out the final report
+	 * 
+	 * @param tokenToCountMap
+	 */
+	private static void printTextAnalyzerReport(
+			Map<String, Integer> tokenToCountMap) {
+		
+		// iterate over the tree map and print the report
+		for(Map.Entry<String, Integer> tokenToCountEntry : tokenToCountMap.entrySet()) {
+			System.out.println(tokenToCountEntry.getValue() + " " + tokenToCountEntry.getKey());
+		}
+		
+	}
+
+}
