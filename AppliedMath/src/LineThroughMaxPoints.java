cbe14ec4dc828c840a9e6f89e77cb412e1761e8d
Praveen Murugesan <lefthandmagic@gmail.com>
2012-09-05 23:43:35 -0700

    format

--- AppliedMath/src/LineThroughMaxPoints.java
+++ AppliedMath/src/LineThroughMaxPoints.java
@@ -6,19 +6,19 @@
  * 
  */
 public class LineThroughMaxPoints {
-	
+
 	class Point {
 		int x;
 		int y;
 	}
-	
+
 	@SuppressWarnings("unused")
 	private static int compute(Point[] points) {
 		int maxVal = 0;
-		for(int i =0; i < points.length; i++) {
-			for(int j=i; j < points.length; j++) {
+		for (int i = 0; i < points.length; i++) {
+			for (int j = i; j < points.length; j++) {
 				int val = numOfPoints(points, points[i], points[j]);
-				if(val > maxVal) {
+				if (val > maxVal) {
 					maxVal = val;
 				}
 			}
@@ -28,17 +28,19 @@ public class LineThroughMaxPoints {
 
 	private static int numOfPoints(Point[] points, Point point1, Point point2) {
 		int count = 0;
-		for(int i=0; i < points.length; i++) {
+		for (int i = 0; i < points.length; i++) {
 			boolean insameline = areInSameLine(point1, point2, points[i]);
-			if(insameline) {
+			if (insameline) {
 				count++;
 			}
 		}
 		return count;
 	}
 
-	private static boolean areInSameLine(Point point1, Point point2, Point point3) {
-		return (point1.x * (point2.y - point3.y) + point2.x * (point3.y - point1.y) + point3.x * (point1.y - point2.y)) == 0;
+	private static boolean areInSameLine(Point point1, Point point2,
+			Point point3) {
+		return (point1.x * (point2.y - point3.y) + point2.x
+				* (point3.y - point1.y) + point3.x * (point1.y - point2.y)) == 0;
 	}
 
 }
